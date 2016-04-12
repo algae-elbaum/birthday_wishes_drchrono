ea7180682ab345ab645db46df964e893c1dc879c
helbaum <helbaum@caltech.edu>
2016-04-12 08:54:37 +0000

    Organized views

---
+++ birthday_wishes_app/site_views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from globs import msg_max, subj_max
+
+def home(request):
+    context = {'logged_in': request.user.is_authenticated()}
+    # Must be a doctor to be here (admins need to pretend to be doctors)
+    if context['logged_in'] and not hasattr(request.user, 'doctor'):
+        return redirect('logout')
+    if context['logged_in']:
+        # Get list of all the user's patients to send out in the response 
+        patient_list = request.user.doctor.get_local_patient_list()
+        context['patient_list'] = patient_list
+    return render(request, 'birthday_wishes.html', context)
+
+@login_required
+def patient_page(request, uid):
+    doctor = request.user.doctor
+    # If it's a POST, set the patient's fields according to the form
+    if request.method == 'POST':
+        activated = request.POST.get('checkbox', False)
+        msg = request.POST['msg']
+        subj = request.POST['subj']
+        time = request.POST['time']
+        doctor.patient_set.filter(uid=uid).update(msg_active = activated, 
+                                                  message = msg[:msg_max], 
+                                                  subject = subj[:subj_max],
+                                                  message_time = time)
+        return redirect(home)
+    # Otherwise send the user to the form
+    else:
+        patient = doctor.patient_set.get(uid=uid)
+        context = {'patient': patient,
+                   'msg_max': msg_max,
+                   'subj_max': subj_max}
+        return render(request, 'patient_page.html', context)
+
+@login_required
+def refresh_patients(request):
+    # If by some hackery the user is logged in but still got to this page 
+    # without authenticating:
+    try:
+        request.user.doctor.update_patient_list()
+        return redirect('home')
+    except:
+        # We don't want to use it as a view, we just want its side effects
+        return redirect ('authorize')
+        request.user.doctor.update_patient_list()
+        return redirect('home')
+
+def about(request):
+    return render(request, 'about.html')
+
--- birthday_wishes_app/urls.py
+++ birthday_wishes_app/urls.py
@@ -2,21 +2,23 @@ from django.conf.urls import url
 from django.contrib.auth.views import login
 from django.views.generic import RedirectView
 
-from . import views
+import site_views, authorization_views, account_views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    url(r'^$', site_views.home, name='home'),
+    url(r'^patient/([0-9]+)', site_views.patient_page, name='patient_page'),
+    url(r'^refresh_patients/', site_views.refresh_patients, name='refresh_patients'),
+    url(r'^about/', site_views.about, name='about'),
+   
+    url(r'^authorize/', authorization_views.authorize, name='authorize'),
+    url(r'^authorization_redirect/', authorization_views.authorization_redirect, name='authorization_redirect'),
+    url(r'^permissions_error/', authorization_views.permissions_error, name='permissions_error'),
+
     url(r'^login/', login, {'template_name': 'login.html'}),
-    url(r'^logout/', views.logout, name='logout'),
-    url(r'^manual_logout/', views.manual_logout, name='manual_logout'),
-    url(r'^register/', views.register, name='register'),
-    url(r'^authorize/', views.authorize, name='authorize'),
-    url(r'^authorization_redirect/', views.authorization_redirect, name='authorization_redirect'),
-    url(r'^permissions_error/', views.permissions_error, name='permissions_error'),
-    url(r'^patient/([0-9]+)', views.patient_page, name='patient_page'),
-    url(r'^refresh_patients/', views.refresh_patients, name='refresh_patients'),
-    url(r'^about/', views.about, name='about'),
-    
+    url(r'^logout/', account_views.logout, name='logout'),
+    url(r'^manual_logout/', account_views.manual_logout, name='manual_logout'),
+    url(r'^register/', account_views.register, name='register'),
+      
     # Not part of the project. I just want this url to still work while django is hogging port 80 
     url(r'^muse_server/$', RedirectView.as_view(url='http://gaster.caltech.edu:8000/muse_server/'))
 ]
